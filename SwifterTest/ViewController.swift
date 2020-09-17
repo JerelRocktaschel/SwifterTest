fe375935bed64c5ab1a5f1229e341e6e464f1528
Jerel Rocktaschel <jerel.rocktaschel@gmail.com>
2020-09-17 16:14:36 -0400

    Initial Commit

---
+++ SwifterTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwifterTest
+//
+//  Created by Jerel Rocktaschel rMBP on 9/17/20.
+//  Copyright © 2020 HighScoreApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
